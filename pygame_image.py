2e6d64a10b9d682a849cabd456af37d8cd897aac
c0c250020d <c0c250020d@edu.teu.ac.jp>
2026-06-09 15:38:48 +0900

    練習3と4:こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")# 練習3
+    kk_img = pg.transform.plip(kk_img,True,False)# 練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(10)# 練習4
 
 
 if __name__ == "__main__":
