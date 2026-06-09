99178334c6f2274803d13ec0eb79230889297abc
c0c250020d <c0c250020d@edu.teu.ac.jp>
2026-06-09 16:20:40 +0900

    練習10:こうかとんの操作

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,30 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)# 練習8
     kk_img = pg.image.load("fig/3.png")# 練習3
     kk_img = pg.transform.flip(kk_img,True,False)# 練習3
+    kk_rct = kk_img.get_rect()# 練習10
+    kk_rct.center = 300,200
+    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])# 練習5:背景画像を右から左へ
         screen.blit(bg_img2, [-x+1600, 0])# 練習7:背景をつなげる
         screen.blit(bg_img, [-x+3200, 0])# 練習9
-        screen.blit(kk_img, [300,200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)# 練習4
